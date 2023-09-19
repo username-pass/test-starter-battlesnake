f10fc34e4fa10bb061bd5290243aa1adabc665e9
username-pass <98624927+username-pass@users.noreply.github.com>
2023-09-19 03:10:30 +0000

    add logic

--- main.go
+++ main.go
@@ -14,7 +14,12 @@ package main
 
 import (
 	"log"
+	// "math"
 	"math/rand"
+	"sort"
+	// TODO: add this:
+	// "gorgonia.org/gorgonia"
+	// "strings"
 )
 
 // info is called when you create your Battlesnake on play.battlesnake.com
@@ -25,10 +30,10 @@ func info() BattlesnakeInfoResponse {
 
 	return BattlesnakeInfoResponse{
 		APIVersion: "1",
-		Author:     "",        // TODO: Your Battlesnake username
-		Color:      "#888888", // TODO: Choose color
-		Head:       "default", // TODO: Choose head
-		Tail:       "default", // TODO: Choose tail
+		Author:     "username-pass", // TODO: Your Battlesnake username
+		Color:      "#888888",       // TODO: Choose color
+		Head:       "default",       // TODO: Choose head
+		Tail:       "default",       // TODO: Choose tail
 	}
 }
 
@@ -64,22 +69,55 @@ func move(state GameState) BattlesnakeMoveResponse {
 	} else if myNeck.X > myHead.X { // Neck is right of head, don't move right
 		isMoveSafe["right"] = false
 
-	} else if myNeck.Y < myHead.Y { // Neck is below head, don't move down
+	}
+	if myNeck.Y < myHead.Y { // Neck is below head, don't move down
 		isMoveSafe["down"] = false
 
 	} else if myNeck.Y > myHead.Y { // Neck is above head, don't move up
 		isMoveSafe["up"] = false
 	}
 
+	log.Print("Width: ", state.Board.Width)
+	log.Print("Height: ", state.Board.Height)
+	log.Print("HeadX: ", myHead.X)
+	log.Print("NeckX: ", myNeck.X)
+	log.Print("HeadY: ", myHead.Y)
+	log.Print("NeckY: ", myNeck.Y)
+
 	// TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-	// boardWidth := state.Board.Width
-	// boardHeight := state.Board.Height
+	boardWidth := state.Board.Width
+	boardHeight := state.Board.Height
+
+	if myHead.X <= 0 {
+		isMoveSafe["left"] = false
+
+	} else if myHead.X >= boardWidth-1 {
+		isMoveSafe["right"] = false
+
+	}
+	if myHead.Y <= 0 {
+		isMoveSafe["down"] = false
+
+	} else if myHead.Y >= boardHeight-1 {
+		isMoveSafe["up"] = false
+	}
 
 	// TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-	// mybody := state.You.Body
+	mybody := state.You.Body
+	log.Print("Body", mybody)
+
+	//check against yourself
+	isMoveSafe = checkBody(isMoveSafe, mybody, myHead)
+	//check against all other snakes
 
 	// TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-	// opponents := state.Board.Snakes
+	opponents := state.Board.Snakes
+	for s := 0; s < len(opponents); s++ {
+		isMoveSafe = checkBody(isMoveSafe, opponents[s].Body, myHead)
+	}
+
+	hazards := state.Board.Hazards
+	isMoveSafe = checkBody(isMoveSafe, hazards, myHead)
 
 	// Are there any safe moves left?
 	safeMoves := []string{}
@@ -89,19 +127,154 @@ func move(state GameState) BattlesnakeMoveResponse {
 		}
 	}
 
+	//TODO: If there are no safe moves left, and there is another snake's head within reach, move there
+
 	if len(safeMoves) == 0 {
 		log.Printf("MOVE %d: No safe moves detected! Moving down\n", state.Turn)
 		return BattlesnakeMoveResponse{Move: "down"}
 	}
 
-	// Choose a random move from the safe ones
-	nextMove := safeMoves[rand.Intn(len(safeMoves))]
-
 	// TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-	// food := state.Board.Food
+	food := state.Board.Food
+
+	log.Print(food)
+	food = nearSort(food, myHead)
+
+	dir := []string{"left", "down"}
+	nearestFood := food[0]
+	dirdist := int2Coord(distboth(nearestFood, myHead))
+	log.Print("Distance to nearest food: ", dirdist)
+
+	if dirdist.X > 0 {
+		dir[0] = "right"
+	} else if dirdist.X == 0 {
+		dir[0] = "straight"
+	}
+	if dirdist.Y > 0 {
+		dir[1] = "up"
+	} else if dirdist.Y == 0 {
+		dir[1] = "straight"
+	}
+	// Choose a random move from the safe ones
+	nextMove := "up"
+
+	var Board = make([][]float32, boardWidth, boardHeight)
+	Board = generateBoard(Board, state)
+	log.Print(Board)
+	if absInt(dirdist.X) < absInt(dirdist.Y) && /* absInt(dirdist.X) > 0 && */ isMoveSafe[dir[0]] {
+		nextMove = dir[0]
+	}
+	if absInt(dirdist.Y) < absInt(dirdist.X) && /* absInt(dirdist.Y) > 0 && */ isMoveSafe[dir[1]] {
+		nextMove = dir[1]
+	}
+
+	if !isMoveSafe[nextMove] {
+		// Choose a random move from the safe ones
+		nextMove = safeMoves[rand.Intn(len(safeMoves))]
+	}
 
 	log.Printf("MOVE %d: %s\n", state.Turn, nextMove)
 	return BattlesnakeMoveResponse{Move: nextMove}
+}
+
+func absInt(x int) int {
+	return absDiffInt(x, 0)
+}
+
+func absDiffInt(x, y int) int {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
+
+func absDiffUint(x, y uint) uint {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
+
+func int2Coord(numarr []int) Coord {
+	tempA := numarr[0]
+	tempB := numarr[1]
+	return Coord{tempA, tempB}
+}
+
+func dist(c1, c2 Coord) int {
+	var dx int = c1.X - c2.X
+	var dy int = c1.Y - c2.Y
+	return dx + dy
+}
+
+func distboth(c1, c2 Coord) []int {
+	var dx int = c1.X - c2.X
+	var dy int = c1.Y - c2.Y
+	return []int{dx, dy}
+}
+
+func nearSort(coords []Coord, head Coord) []Coord {
+	sort.Slice(coords, func(i, j int) bool {
+		return dist(coords[i], head) < dist(coords[j], head)
+	})
+	return coords
+}
+
+func lerp(A, B, C float32) float32 {
+	return A*(1-C) + B*(C)
+}
+
+func generateBoard(Board [][]float32, state GameState) [][]float32 {
+
+	var head Coord = state.You.Head
+
+	//set default values on Board
+	for x := 0; x < state.Board.Width; x++ {
+		for y := 0; y < state.Board.Width; y++ {
+			Board[x][y] = 0
+		}
+	}
+
+	//iterate through all of your character's positions:
+	var bodyCostMax float32 = -50
+	var bodyCostMin float32 = -3
+	for i, coord := range state.You.Body {
+		var cost float32 = lerp(bodyCostMin, bodyCostMax, float32(i/len(state.You.Body)))
+		Board[coord.X][coord.Y] = cost
+	}
+
+	//iterate through all the food on the board
+	var foodCostMax float32 = 10
+	var foodCostMin float32 = 1
+	var food []Coord = nearSort(state.Board.Food, head)
+	for i, coord := range food {
+		//cost based on distance
+		var cost float32 = lerp(foodCostMin, foodCostMax, float32(i/len(food)))
+		Board[coord.X][coord.Y] = cost
+	}
+
+	return Board
+}
+
+func checkBody(issafe map[string]bool, body []Coord, head Coord) map[string]bool {
+	for i := 0; i < len(body); i++ {
+		bodycell := body[i]
+
+		if (head.X-1) == bodycell.X && head.Y == bodycell.Y {
+			issafe["left"] = false
+		}
+		if (head.X+1) == bodycell.X && head.Y == bodycell.Y {
+			issafe["right"] = false
+		}
+		if (head.Y-1) == bodycell.Y && head.X == bodycell.X {
+			issafe["down"] = false
+		}
+		if (head.Y+1) == bodycell.Y && head.X == bodycell.X {
+			issafe["up"] = false
+		}
+
+	}
+	return issafe
 }
 
 func main() {
